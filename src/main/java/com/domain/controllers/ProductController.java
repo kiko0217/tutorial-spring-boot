fff05ff3af73d1e35a8431daac688b4d3ab46fd3
kiko0217 <kiko0217@github>
2022-04-14 08:27:40 +0700

    penambahan beberapa logger

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -1,10 +1,17 @@
 package com.domain.controllers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.validation.Valid;
+
 import com.domain.models.entities.Product;
 import com.domain.models.entities.ProductRequestModel;
 import com.domain.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,11 +27,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 public class ProductController {
     
+    /**
+     *
+     */
+    private static final RuntimeException RUNTIME_EXCEPTION = new RuntimeException("Validataion Error");
     @Autowired
     private ProductService productService;
 
+    Logger logger = Logger.getLogger(
+            ProductController.class.getName());
+
     @PostMapping
-    public Product create(@RequestBody ProductRequestModel productRequest){
+    public Product create(@Valid @RequestBody ProductRequestModel productRequest, Errors errors){
+        if(errors.hasErrors()){
+            for(ObjectError error: errors.getAllErrors()){
+                logger.log(Level.INFO, error.getDefaultMessage());
+            }
+            throw RUNTIME_EXCEPTION;
+        }
         Product product = new Product(productRequest.getId(),productRequest.getName(),productRequest.getDescription(),productRequest.getPrice());
         return productService.save(product);
     }
